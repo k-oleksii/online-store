354bdba46e9caa07e1e301ba34d011aa57c7dc06
Aleksey Kozyuberda <kozyuberda.aleksey@gmail.com>
2023-07-05 17:22:50 +0300

    Base page

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { CSSProperties } from 'react';
+import logo from './assets/logo.svg';
 
-function App() {
-  const [count, setCount] = useState(0)
+export const App = () => {
+  const styles: CSSProperties = {
+    padding: '10px 20px',
+    textAlign: 'center',
+    color: '#000',
+    fontSize: '22px',
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App
+    <div style={styles}>
+      <a href='#' target='_blank'>
+        <img src={logo} className='logo react' alt='React logo' />
+      </a>
+      <h1>Welcome to the online store CityWheel!</h1>
+    </div>
+  );
+};
