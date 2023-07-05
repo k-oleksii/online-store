b1e713a00bd46454f38194c150dd1c454413a51e
Aleksey Kozyuberda <kozyuberda.aleksey@gmail.com>
2023-07-05 23:35:40 +0300

    Added plugin Material UI, Tailwaind and Styled-companents

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import macrosPlugin from 'vite-plugin-babel-macros';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), macrosPlugin()],
   base: '/online-store/',
   server: {
     port: 3000,
