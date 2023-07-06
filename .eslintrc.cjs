85702ecc9c5a3a5ec189ec5cf9743c0976fb7679
Aleksey Kozyuberda <kozyuberda.aleksey@gmail.com>
2023-07-06 13:27:09 +0300

    Added eslint and prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,13 +2,59 @@ module.exports = {
   env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
+    'plugin:react/recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
   ],
   parser: '@typescript-eslint/parser',
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  plugins: ['react-refresh'],
+  parserOptions: {
+    ecmaVersion: '2021',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: ['react-refresh', 'react', 'react-hooks', '@typescript-eslint'],
   rules: {
     'react-refresh/only-export-components': 'warn',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'react/react-in-jsx-scope': 'off',
   },
-}
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
+
+// module.exports = {
+//   env: {
+//     browser: true,
+//     es2020: true,
+//   },
+//   extends: [
+//     'eslint:recommended',
+//     'plugin:react/recommended',
+//     'plugin:@typescript-eslint/recommended',
+//   ],
+//   parser: '@typescript-eslint/parser',
+//   parserOptions: {
+//     ecmaVersion: 'latest',
+//     sourceType: 'module',
+//     ecmaFeatures: {
+//       jsx: true,
+//     },
+//   },
+//   plugins: ['react-refresh', 'react', 'react-hooks', '@typescript-eslint'],
+//   rules: {
+//     'react-refresh/only-export-components': 'warn',
+//     'react-hooks/rules-of-hooks': 'error',
+//     'react-hooks/exhaustive-deps': 'warn',
+//   },
+//   settings: {
+//     react: {
+//       version: 'detect',
+//     },
+//   },
+// };
