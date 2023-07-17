b334e92ac05c96e6b58f2e4019533ffb80651c4c
Aleksey Kozyuberda <kozyuberda.aleksey@gmail.com>
2023-07-17 14:26:41 +0300

    Added colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,19 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    colors: {
+      main: {
+        black: '#212121',
+        red: '#dd2255',
+      },
+      secondary: {
+        bg: '#f8faff',
+        cardBg: '#ffffff',
+        gray: '#878d99',
+        blue: '#8083ff',
+        green: '#008e24',
+      },
+    },
     extend: {},
   },
   plugins: [],
